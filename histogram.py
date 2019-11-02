e77855e94ee2659888efb89bb3cdb910da511a9f
M. Imam Pratama <imambungo@gmail.com>
2019-11-02 22:20:41 +0700

    pisahkan RGB gambar ke tiga citra terpisah

--- histogram.py
+++ histogram.py
@@ -8,6 +8,15 @@ def histogram(gambar):
     ukuran_horizontal = GAMBAR.size[0]
     ukuran_vertikal = GAMBAR.size[1]
 
+    gambar_r = Image.new('RGB', (ukuran_horizontal, ukuran_vertikal))
+    pixel_r = gambar_r.load()
+
+    gambar_g = Image.new('RGB', (ukuran_horizontal, ukuran_vertikal))
+    pixel_g = gambar_g.load()
+
+    gambar_b = Image.new('RGB', (ukuran_horizontal, ukuran_vertikal))
+    pixel_b = gambar_b.load()
+
     # https://stackoverflow.com/q/10712002/9157799
     r = [0] * 256
     g = [0] * 256
@@ -21,6 +30,13 @@ def histogram(gambar):
             r[intensitas_r] += 1
             g[intensitas_g] += 1
             b[intensitas_b] += 1
+            pixel_r[x, y] = (intensitas_r, 0, 0)
+            pixel_g[x, y] = (0, intensitas_g, 0)
+            pixel_b[x, y] = (0, 0, intensitas_b)
+
+    gambar_r.save('red.jpg')
+    gambar_g.save('green.jpg')
+    gambar_b.save('blue.jpg')
 
     intensitas = list(range(256))
     lebar_bar = 0.27

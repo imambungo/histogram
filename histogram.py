4f8f77e7883e1e681aab11e865cc2d2056f6dc1c
M. Imam Pratama <imambungo@gmail.com>
2019-11-02 22:03:06 +0700

    modifikasi fungsi histogram untuk menerima gambar sebagai argumen

--- histogram.py
+++ histogram.py
@@ -1,24 +1,46 @@
 from matplotlib import pyplot
+from PIL import Image
 
-def histogram(R, G, B):
-    "menampilkan histogram berdasarkan kemunculan setiap intensitas RGB"
-    intensitas = list(range(11))
+def histogram(gambar):
+    GAMBAR = Image.open(gambar)
+    PIXEL = GAMBAR.load()
+
+    ukuran_horizontal = GAMBAR.size[0]
+    ukuran_vertikal = GAMBAR.size[1]
+
+    # https://stackoverflow.com/q/10712002/9157799
+    r = [0] * 256
+    g = [0] * 256
+    b = [0] * 256
+
+    for x in range(ukuran_horizontal):
+        for y in range(ukuran_vertikal):
+            intensitas_r = PIXEL[x, y][0]
+            intensitas_g = PIXEL[x, y][1]
+            intensitas_b = PIXEL[x, y][2]
+            r[intensitas_r] += 1
+            g[intensitas_g] += 1
+            b[intensitas_b] += 1
+
+    intensitas = list(range(256))
     lebar_bar = 0.27
+
+    # https://stackoverflow.com/q/9304408/9157799
     intensitas = [i-lebar_bar for i in intensitas]
-    pyplot.bar(intensitas, R, width=lebar_bar, color='r')
+    # https://stackoverflow.com/q/14270391/9157799
+    pyplot.bar(intensitas, r, width=lebar_bar, color='r')
+
     intensitas = [i+lebar_bar for i in intensitas]
-    pyplot.bar(intensitas, G, width=lebar_bar, color='g')
+    pyplot.bar(intensitas, g, width=lebar_bar, color='g')
+
     intensitas = [i+lebar_bar for i in intensitas]
-    pyplot.bar(intensitas, B, width=lebar_bar, color='b')
-    pyplot.title('Histogram')
+    pyplot.bar(intensitas, b, width=lebar_bar, color='b')
+
+    pyplot.title('Histogram ' + gambar)
     pyplot.xlabel('Intensitas')
     pyplot.ylabel('Kemunculan')
     pyplot.legend(['R', 'G', 'B'])
     pyplot.show()
 
 
-R = [1, 4, 9, 4, 5, 8, 6, 7, 8, 9, 10]
-G = [10, 5, 2, 4, 5, 7, 6, 7, 8, 9, 10]
-B = [10, 5, 2, 4, 5, 7, 6, 7, 8, 9, 10]
-
-histogram(R, G, B)
+histogram('gambar.jpg')
